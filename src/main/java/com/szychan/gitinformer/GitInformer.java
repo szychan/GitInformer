493276d9d234d62bd38c76509627fca8ee0d8f64
szychan <szychan@gmail.com>
2017-06-17 21:56:02 +0200

    Added rest client for getting user info

--- src/main/java/com/szychan/gitinformer/GitInformer.java
+++ src/main/java/com/szychan/gitinformer/GitInformer.java
@@ -1,9 +1,37 @@
 package com.szychan.gitinformer;
 
+import javax.json.JsonArray;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+import org.glassfish.jersey.jsonp.JsonProcessingFeature;
+
 public class GitInformer {
 
 	public static void main(String[] args) {
 
+		if(args.length!=1){
+			System.exit(0);
+		}
+		
+		JsonArray jsonArray = getUserRepositories(args[0]);
+		//System.out.println(jsonArray.toString());
 	}
+	
+	public static JsonArray getUserRepositories(String userName){
+		JsonArray jsonArray = null;
 
+    		Client client = ClientBuilder.newClient();
+    	    
+            WebTarget target = client.register(JsonProcessingFeature.class).target("https://api.github.com").path("users/"+userName+"/repos").queryParam("sort","updated");
+            System.out.println(target.getUri());
+       
+            jsonArray = target.request(MediaType.APPLICATION_JSON_TYPE).get(JsonArray.class);
+
+
+
+    	return jsonArray;
+	}
 }
